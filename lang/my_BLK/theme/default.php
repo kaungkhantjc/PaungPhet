2d535bfb8afaee4c0f2d37c5b0e4f0664f776d4c
Khun Htetz Naing <khunhtetznaing@gmail.com>
2025-12-07 21:40:00 +0630

    Fix typo in Pa'O translations

--- lang/my_BLK/theme/default.php
+++ lang/my_BLK/theme/default.php
@@ -3,7 +3,7 @@
 return [
     'title' => ':partner_one တွမ်ႏ :partner_two သီး ပွယ်ꩻမင်္ဂလာႏစေနေင်းယာ',
     'event_subtitle' => 'သီး ပွယ်ꩻမင်္ဂလာႏစေနေင်းယာ 👩‍❤️‍👨',
-    'all_invitees_subtitle' => 'ဖစ်လောဝ်းဟွိုန်ဒျာႏ ကားကအဝ်ႏဩ 💛',
+    'all_invitees_subtitle' => 'ကားကအဝ်ႏ ဖစ်လောဝ်းဟွိုန်ဒျာႏဩ 💛',
     'invitee_subtitle' => 'ဖစ်လောဝ်းဟွိုန်ဒျာႏဩ 💛',
     'content_title' => 'စေနေင်းယာလိတ်ဖစ်',
     'prewedding_images_title' => 'အမတ်တရဓာတ်ပွုံꩻဖုံႏ',
